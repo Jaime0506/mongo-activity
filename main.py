9f5b9bd17e9a6f62846ad774f3f3e0422d22b492
Jaime Mejia <jaimemejia1003@gmail.com>
2025-08-31 13:33:29 -0500

    config to deploy

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from config.database import connect_to_mongo, close_mongo_connection
 from contextlib import asynccontextmanager
 from routes.router_car import router as router_car
+import os
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -21,3 +22,8 @@ app.include_router(router_car)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
